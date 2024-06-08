88f6471a813cbbdbfd37fbd92f304f3afbe3f21b
Vic K <kondratskayavictoria@gmail.com>
2024-06-08 19:02:43 +0300

    added heroku configs

--- eachyearnote/settings.py
+++ eachyearnote/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(#!(z4b(=1+^=ifrp4#s68&q^qg4!9o9!4lo#-+_irqnc-(&5m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
