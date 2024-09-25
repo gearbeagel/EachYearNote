441e8b4f998f16fb8c0b4143d649a99977751011
Vic K <kondratskayavictoria@gmail.com>
2024-09-25 22:36:02 +0300

    version 2: 20

--- main/views.py
+++ main/views.py
@@ -6,8 +6,8 @@ from main.models import YearlyCards
 # Create your views here.
 def home(request):
     cards = YearlyCards.objects.all().order_by('-year')
-    messages.info(request, 'Переверни телефон так, як в бравл граєш!')
     return render(request, 'home.html', {'cards': cards})
+
 
 def card_detail(request, slug):
     card = get_object_or_404(YearlyCards, slug=slug)
